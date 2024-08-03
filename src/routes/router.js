94c985c049e70a1be10cbac55d679aec859a9e1f
Alexploto <mascadoaperro@gmail.com>
2024-08-02 21:21:22 -0300

    Estoy empezando el back. Arquitectura de carpetas.

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,14 +3,14 @@
 
 
 const checkUserTypesMw = require(`../utils/checkUserTypesMw.js`)
-const {addcardController} = require(`../controllers/addcardController.js`)
 const {adduserController} = require(`../controllers/adduserController.js`)
 const express = require(`express`)
 const router = express.Router()
 router.get (`/prueba`,(__,res)=>{
 res.send(`bienvenido`)    
 })
-router.post(`/addcard`,checkUserTypesMw, addcardController)	
+router.post(`/addcard`,checkUserTypesMw, adduserController)
+router.get	
 
 
 module.exports = router;
